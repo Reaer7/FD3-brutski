13e6ac8253975c8c029162d32643e4efa6023ca3
Brutski <last.reater@gmail.com>
2024-01-02 15:11:13 +0300

    add some beauty

---
+++ hw4-mouse-and-timers/src/components/common/Spinner.jsx
@@ -0,0 +1,3 @@
+export function Spinner() {
+	return <img src="/logo.svg" className="App-logo spinner" alt="spinner" />;
+}
--- hw4-mouse-and-timers/src/components/pages/StartPage.jsx
+++ hw4-mouse-and-timers/src/components/pages/StartPage.jsx
@@ -1,8 +1,10 @@
 import '../App.css';
+import { Spinner } from "../common/Spinner";
 
 export function StartPage() {
 	return <>
 		<h3>This is Start Page!</h3>
 		<div>Use header link</div>
+		<Spinner />
 	</>
 }
