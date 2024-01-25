b549e59e162aad3834b69f2241f3a286ef7dab56
Artur <last.reater@gmail.com>
2024-01-25 19:22:39 +0300

    change redirect to new tab by photo.url

--- hw5-placeholded-albums/src/components/common/LittleCard.jsx
+++ hw5-placeholded-albums/src/components/common/LittleCard.jsx
@@ -6,17 +6,15 @@
  */
 export function LittleCard(data) {
     return <>
-    <a href={data.url} target='_blank'>
-        <div className="same-card"
-            // onClick={useCallback(() => window.open(data.url, '_blank', 'noopener noreferrer'),[data])}
-            // onClick={() => window.open(data.url, '_blank', 'noopener noreferrer')}
-        >
+        <div className="same-card" onClick={!!data.url
+            ? () => window.open(data.url, '_blank', 'noopener noreferrer')
+            : () => {}
+        }>
             <img src={data.thumbnailUrl}
                  alt="little photo"
                  border="1px blue solid"
             />
             <div className="text">{data.title}</div>
         </div>
-    </a>
     </>
 }
