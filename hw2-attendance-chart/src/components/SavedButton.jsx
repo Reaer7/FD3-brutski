fa6f18431b276af52adf59b972b87099875af7bd
Artur <last.reater@gmail.com>
2024-02-01 20:29:00 +0300

    simplify code 'SavedButton'

--- hw2-attendance-chart/src/components/SavedButton.jsx
+++ hw2-attendance-chart/src/components/SavedButton.jsx
@@ -4,34 +4,25 @@ const LOADING_TIME_MS = 2000;
 
 export function SavedButton({ data }) {
 	const [isClicked, setIsClick] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
-	const [isDisabled, setIsDisabled] = useState(false);
-	const [text, setText] = useState("Save");
 
 	useEffect(() => {
 		if (isClicked) {
-			setIsLoading(!isLoading);
-			setIsDisabled(true);
-			setText("Saving in progress");
 
 			setTimeout(() => {
 				data.forEach((keyValue) => localStorage.setItem(
 					keyValue.key,
 					JSON.stringify(keyValue?.value)
 				));
-				setIsClick(!isClicked);
-				setIsLoading(!isLoading);
-				setIsDisabled(false);
-				setText("Save");
+				setIsClick((prevState) => !prevState);
 			}, LOADING_TIME_MS);
 		}
 	}, [isClicked]);
 
 	return <button
 		className="wideButton"
-		onClick={() => setIsClick(!isClicked)}
-		disabled={isDisabled}
+		onClick={() => setIsClick((prevState) => !prevState)}
+		disabled={isClicked}
 	>
-		{text}
+		{isClicked ? 'Saving in progress' : 'Save'}
 	</button>
 }
